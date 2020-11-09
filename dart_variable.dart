d3e560dd03fef6d0ba59a6e2c8cbaf6f6c2a6e43
JaimeCerseiLannister <zhx1222@foxmail.com>
2020-11-09 22:27:07 +0800

    DART

---
+++ dart_variable.dart
@@ -0,0 +1,24 @@
+void main() {
+  /// 初始化一个字符串
+  var name = 'Fulade';
+
+  /// Int类型
+  var year = 1987;
+
+  /// 浮点数类型
+  var antennaDiameter = 3.7;
+
+  /// 数组
+  var flybyObjects = ['木星', '土星', '天王星', '海王星'];
+
+  /// 字典类型
+  var image = {
+    'tags': ['土星'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  int lineCount;
+  if (lineCount == null) {
+    print("line is null");
+  }
+}
