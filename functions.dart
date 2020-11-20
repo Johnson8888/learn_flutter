06d779876054592cfa90432ea50d07545f70e8e1
Johnson8888 <zhx1222@foxmail.com>
2020-11-20 17:31:28 +0800

    functions

---
+++ functions.dart
@@ -0,0 +1,24 @@
+/*
+ * @Author: 弗拉德
+ * @Date: 2020-11-20 17:13:12
+ * @LastEditTime: 2020-11-20 17:22:00
+ * @Support: http://fulade.me
+ */
+
+/*  //带有返回值的写法
+bool isEmpty(List aList) {
+  return aList.length == 0;
+}
+*/
+
+/*
+/// 简单写法
+bool isEmpty(List aList) => aList == 0;
+*/
+main(List<String> args) {
+  /*
+  var aList = [];
+  bool aa = isEmpty(aList);
+  print("the list $aList is empty = " + aa.toString());
+  */
+}
