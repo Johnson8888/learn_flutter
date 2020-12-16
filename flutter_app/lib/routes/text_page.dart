2e11129a9d798b86e8ec364f6e71bbe9d322e5ce
Johnson8888 <zhx1222@foxmail.com>
2020-12-16 15:23:59 +0800

    text page

--- flutter_app/lib/routes/text_page.dart
+++ flutter_app/lib/routes/text_page.dart
@@ -1,9 +1,10 @@
 /*
  * @Author: 弗拉德
  * @Date: 2020-12-16 08:36:42
- * @LastEditTime: 2020-12-16 09:17:05
+ * @LastEditTime: 2020-12-16 14:59:53
  * @Support: http://fulade.me
  */
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class TextDemoPage extends StatefulWidget {
@@ -12,16 +13,115 @@ class TextDemoPage extends StatefulWidget {
 }
 
 class _TextDemoPageState extends State<TextDemoPage> {
+  var tapCount = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Text Demo"),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
-      body: Text("Text Demo"),
-    );
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("Text Demo"),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Fulade"),
+                Text(
+                  "粗体",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "红色",
+                  style: TextStyle(color: Colors.red),
+                ),
+                Text("字号20", style: TextStyle(fontSize: 20)),
+                Text(
+                  "斜体",
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+                Text(
+                  "文字放大1.5倍",
+                  textScaleFactor: 1.5,
+                ),
+                Text(
+                  "删除线",
+                  style: TextStyle(
+                      decoration: TextDecoration.lineThrough,
+                      decorationColor: Colors.red),
+                ),
+                Text(
+                  "下划线",
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                Text(
+                  "居右显示" * 10,
+                  textAlign: TextAlign.right,
+                ),
+                Text(
+                  "最多一行显示超过部分显示..." * 10,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: "多彩文字：",
+                    style: TextStyle(color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: "红色、", style: TextStyle(color: Colors.red)),
+                      TextSpan(
+                          text: "蓝色、", style: TextStyle(color: Colors.blue)),
+                      TextSpan(
+                          text: "绿色 ", style: TextStyle(color: Colors.green)),
+                    ],
+                  ),
+                ),
+                RichText(
+                    text: TextSpan(
+                  text: "添加了手势的文字: ",
+                  style: TextStyle(color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: "点击会输出Log",
+                      style: TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          tapCount++;
+                          print("taped taped " + tapCount.toString());
+                        },
+                    ),
+                  ],
+                )),
+              ],
+            ),
+            DefaultTextStyle(
+              //1.设置文本默认样式
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 20.0,
+              ),
+              textAlign: TextAlign.start,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("DefaultTextStyle "),
+                  Text("DefaultTextStyle"),
+                  Text(
+                    "改变了的DefaultTextStyle",
+                    style: TextStyle(
+                        inherit: false, //设置不再继承默认样式
+                        color: Colors.blue),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
