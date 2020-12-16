52bce2ac3638a2e659c8c6ded3ed53a7ad835bbd
Johnson8888 <zhx1222@foxmail.com>
2020-12-16 20:28:57 +0800

    modify readme

--- flutter_app/lib/routes/text_page.dart
+++ flutter_app/lib/routes/text_page.dart
@@ -1,7 +1,7 @@
 /*
  * @Author: 弗拉德
  * @Date: 2020-12-16 08:36:42
- * @LastEditTime: 2020-12-16 14:59:53
+ * @LastEditTime: 2020-12-16 19:31:38
  * @Support: http://fulade.me
  */
 import 'package:flutter/gestures.dart';
@@ -31,6 +31,19 @@ class _TextDemoPageState extends State<TextDemoPage> {
               children: [
                 Text("Fulade"),
                 Text(
+                  "文字放大1.5倍",
+                  textScaleFactor: 1.5,
+                ),
+                Text(
+                  "居右显示" * 10,
+                  textAlign: TextAlign.right,
+                ),
+                Text(
+                  "最多一行显示超过部分显示..." * 10,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
                   "粗体",
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
@@ -38,14 +51,19 @@ class _TextDemoPageState extends State<TextDemoPage> {
                   "红色",
                   style: TextStyle(color: Colors.red),
                 ),
-                Text("字号20", style: TextStyle(fontSize: 20)),
+                Text(
+                  "字号20",
+                  style: TextStyle(fontSize: 20),
+                ),
                 Text(
                   "斜体",
                   style: TextStyle(fontStyle: FontStyle.italic),
                 ),
                 Text(
-                  "文字放大1.5倍",
-                  textScaleFactor: 1.5,
+                  "背景颜色红色",
+                  style: TextStyle(
+                      background: Paint()..color = Colors.red,
+                      color: Colors.blue),
                 ),
                 Text(
                   "删除线",
@@ -59,16 +77,8 @@ class _TextDemoPageState extends State<TextDemoPage> {
                     decoration: TextDecoration.underline,
                   ),
                 ),
-                Text(
-                  "居右显示" * 10,
-                  textAlign: TextAlign.right,
-                ),
-                Text(
-                  "最多一行显示超过部分显示..." * 10,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
                 RichText(
+                  textAlign: TextAlign.center,
                   text: TextSpan(
                     text: "多彩文字：",
                     style: TextStyle(color: Colors.black),
@@ -83,21 +93,22 @@ class _TextDemoPageState extends State<TextDemoPage> {
                   ),
                 ),
                 RichText(
-                    text: TextSpan(
-                  text: "添加了手势的文字: ",
-                  style: TextStyle(color: Colors.black),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: "点击会输出Log",
-                      style: TextStyle(color: Colors.blue),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          tapCount++;
-                          print("taped taped " + tapCount.toString());
-                        },
-                    ),
-                  ],
-                )),
+                  text: TextSpan(
+                    text: "添加了手势的文字: ",
+                    style: TextStyle(color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "点击会输出Log",
+                        style: TextStyle(color: Colors.blue),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            tapCount++;
+                            print("taped taped " + tapCount.toString());
+                          },
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
             DefaultTextStyle(
